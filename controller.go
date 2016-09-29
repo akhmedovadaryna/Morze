23a0420ad94ab51020d8ed390dc7b0d18a2f075b
akhmedovadaryna <darina.akhmedova@gmail.com>
2016-09-29 22:35:31 +0300

    Add files via upload

--- controller.go
+++ controller.go
@@ -8,13 +8,23 @@ import (
 
 func main() {
 	//var text2 string = ".-___.-_.-_.-___.-"
-	var name_file = "input.txt"
+	var name_file = "./files/input_text"
 	text, err := io.Input_from_file(name_file)
 	if err != nil {
         	return
     	}
 	result := Morze.TextToMorze(text)
 	fmt.Println(result)
+
+
+	var filename = "./files/output.txt"
+	err = nil
+	err = io.Output_from_file(filename,result)
+	if err != nil {
+		fmt.Println("error")
+		return
+	}
+
 	result = Morze.Morze(result)
 	fmt.Println(result)
 
