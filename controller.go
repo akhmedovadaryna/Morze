f6c230e45de3ac37117ccabb872cdedcf1770c99
Alexandr <ivanov.alexandr.1995@gmail.com>
2016-09-30 01:29:03 +0300

    some

--- controller.go
+++ controller.go
@@ -1,31 +1,60 @@
-package Morze
+package main
 
 import (
 	"./logic"
 	"./io"
 	"fmt"
+	"flag"
 )
 
 func main() {
-	//var text2 string = ".-___.-_.-_.-___.-"
-	var name_file = "./files/input_text"
-	text, err := io.Input_from_file(name_file)
-	if err != nil {
-        	return
-    	}
-	result := logic.TextToMorze(text)
-	fmt.Println(result)
+
+	m := flag.String("morze", "", "morze to text")
+	t := flag.String("text", "", "text to morze")
+	flag.Parse()
 
 
-	var filename = "./files/output.txt"
-	err = nil
-	err = io.Output_from_file(filename,result)
-	if err != nil {
-		fmt.Println("error")
-		return
+	if *t != "" {
+
+		var name_file = "./files/"
+		name_file += *t
+		fmt.Println(*t)
+		text, err := io.Input_from_file(name_file)
+		if err != nil {
+			return
+		}
+		result := logic.TextToMorze(text)
+		fmt.Println(result)
+
+		var filename = "./files/output.txt"
+		err = nil
+		err = io.Output_from_file(filename, result)
+		if err != nil {
+			fmt.Println("error")
+			return
+		}
+	}
+	if *m != "" {
+
+		var name_file = "./files/"
+		name_file += *m
+		fmt.Println(*m)
+		text, err := io.Input_from_file(name_file)
+		if err != nil {
+			return
+		}
+		result := logic.Morze(text)
+		fmt.Println(result)
+
+		var filename = "./files/output.txt"
+		err = nil
+		err = io.Output_from_file(filename, result)
+		if err != nil {
+			fmt.Println("error")
+			return
+		}
+
 	}
 
-	result = logic.Morze(result)
-	fmt.Println(result)
 
 }
--- controller_test.go
+++ controller_test.go
@@ -1,4 +1,4 @@
-package Morze
+package main
 
 import "testing"
 
