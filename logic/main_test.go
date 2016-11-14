3fe15117ca3ef4c4b9c9d7056702735909bb2902
VadymGrygoriev <gawrikon@gmail.com>
2016-11-14 22:21:35 +0200

    Add files via upload

--- logic/main_test.go
+++ logic/main_test.go
@@ -5,31 +5,11 @@ import (
 	"fmt"
 )
 
-func TestMorze(t *testing.T) {
+func TestFormat_text(t *testing.T) {
 	var m string
-	m = Morze("--._---____--._._-_")
+	m = format_text("AAAAA.....AAAAA")
 	fmt.Printf(m)
-	if m != "GO GET"{
-		t.Error("Expected GO GET, got", m)
-	}
-}
-
-func TestTextToMorze(t *testing.T) {
-	var ttm string
-	ttm = TextToMorze("Go")
-	if ttm != "--._---_\n" {
-		t.Error("Expecteg --._---_, got", ttm)
-	}
-}
-
-func BenchmarkMorze(b *testing.B) {
-	for n := 0; n < b.N; n++{
-		Morze("--._---____--._._-_")
-	}
-}
-func BenchmarkTextToMorze(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		TextToMorze("GO GET")
-
+	if m != "AAAAAAAAAA"{
+		t.Error("Expected AAAAAAAAAA, got", m)
 	}
 }
