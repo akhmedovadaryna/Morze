9dd4bc2674126ccd986d76fc40340bdb57bb1918
VadymGrygoriev <gawrikon@gmail.com>
2016-11-14 22:21:21 +0200

    Add files via upload

--- io/io_test.go
+++ io/io_test.go
@@ -2,9 +2,16 @@ package io
 
 import "testing"
 
-func TestInput_from_file(t *testing.T) {
-	iff, err := Input_from_file("C:/Users/Дарина/PycharmProjects/Morze/files/input_text")
-	if iff != "Go" {
-		t.Error("Expected Go", iff , err)
+func TestSerial_Input_from_file(t *testing.T) {
+	siff, err := Serial_Input_from_file("input_text1")
+	if siff != "Rose in Bloom" {
+		t.Error("Expected Rose in Bloom", siff , err)
+	}
+}
+
+func TestSerial_Output_from_file(t *testing.T) {
+	soff := Serial_Output_from_file("output", "res")
+	if soff == nil {
+		t.Error("Expected not nil, got ", soff)
 	}
 }
